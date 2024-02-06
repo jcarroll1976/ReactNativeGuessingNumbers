18ac3813b5f970ba1f3e90c4218511a90a451993
Joshua Carroll <carrolljosh1976@gmail.com>
2024-02-05 22:11:24 -0500

    Finished styling buttons for StartGameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -10,14 +10,22 @@ export default function StartGameScreen() {
              keyboardType="number-pad"
              autoCapitalize="none"
              autoCorrect={false} />
-                <PrimaryButton>Reset</PrimaryButton>
-                <PrimaryButton>Confirm</PrimaryButton>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
+        justifyContent: "center",
+        alignItems: "center",
         marginTop: 100,
         marginHorizontal: 24,
         padding: 16,
@@ -39,5 +47,11 @@ const styles = StyleSheet.create({
         color: "#ddb52f",
         fontWeight: "bold",
         textAlign: "center"
+    },
+    buttonsContainer: {
+        flexDirection: "row"
+    },
+    buttonContainer: {
+        flex: 1
     }
 });
