313948802d201ef9c9b2d4617859271985ef318f
Joshua Carroll <carrolljosh1976@gmail.com>
2024-02-12 19:31:47 -0500

    Added some functionality to switch to GameScreen if user chooses valid number

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { TextInput,View,StyleSheet,Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-export default function StartGameScreen() {
+export default function StartGameScreen({onPickNumber}) {
     const [enteredNumber,setEnteredNumber] = useState("");
 
     function numberInputHandler(enteredText) {
@@ -24,7 +24,7 @@ export default function StartGameScreen() {
             return;
         }
 
-        console.log("Valid number!")
+        onPickNumber(chosenNumber);
     }
 
     return (
