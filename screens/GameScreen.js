d2d55ab72d7bc252b39eff056fa63fcbe5d28352
Joshua Carroll <carrolljosh1976@gmail.com>
2024-02-13 22:21:54 -0500

    Started building GameScreen component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,22 @@
-import { Text } from "react-native"
+import { Text,View,StyleSheet } from "react-native"
 
  export default function GameScreen() {
     return (
-        <Text>Game Screen!</Text>
+        <View style={styles.screen}>
+            <Text>Game Screen!</Text>
+            {/*GUESS*/}
+            <View>
+                <Text>Higher or lower?</Text>
+                {/*+ -*/}
+            </View>
+            {/*<View>LOG ROUNDS</View>*/}
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 12
+    }
+})
