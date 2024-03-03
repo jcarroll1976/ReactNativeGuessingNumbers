3d3f0ca61aedef56df56c18c576c1c77f3fd66e7
Joshua Carroll <carrolljosh1976@gmail.com>
2024-03-02 23:56:19 -0500

    Added KeyboardAvoidingView component to manage screen content while using device keyboard

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { TextInput,View,StyleSheet,Alert,useWindowDimensions } from "react-native";
+import { TextInput,
+         View,
+         StyleSheet,
+         Alert,
+         useWindowDimensions,
+         KeyboardAvoidingView,
+         ScrollView } from "react-native";
 
 import Colors from "../utils/colors";
 
@@ -38,34 +44,41 @@ export default function StartGameScreen({onPickNumber}) {
     const marginTop = height < 380 ? 30 : 100;
 
     return (
-        <View style = {[styles.rootContainer, {marginTop: marginTop}]}>
-            <Title>Guess The Number</Title>
-            <Card>
-                <InstructionText>Enter A Number</InstructionText>
-                <TextInput
-                style = {styles.numberInput}
-                maxLength={2}
-                keyboardType="number-pad"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={numberInputHandler}
-                value={enteredNumber} />
-                <View style={styles.buttonsContainer}>
-                    <View style={styles.buttonContainer}>
-                        <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
-                    </View>
-                    <View style={styles.buttonContainer}>
-                        <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
-                    </View>
+        <ScrollView style = {styles.screen}>
+            <KeyboardAvoidingView style = {styles.screen} behavior="position">
+                <View style = {[styles.rootContainer, {marginTop: marginTop}]}>
+                    <Title>Guess The Number</Title>
+                    <Card>
+                        <InstructionText>Enter A Number</InstructionText>
+                        <TextInput
+                        style = {styles.numberInput}
+                        maxLength={2}
+                        keyboardType="number-pad"
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        onChangeText={numberInputHandler}
+                        value={enteredNumber} />
+                        <View style={styles.buttonsContainer}>
+                            <View style={styles.buttonContainer}>
+                                <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+                            </View>
+                            <View style={styles.buttonContainer}>
+                                <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+                            </View>
+                        </View>
+                    </Card>
                 </View>
-            </Card>
-        </View>
+            </KeyboardAvoidingView>
+        </ScrollView>
     )
 }
 
 //const deviceHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
+    screen: {
+        flex: 1
+    },
     rootContainer: {
         flex: 1,
         //marginTop: deviceHeight < 380 ? 30 : 100,
