6cb0897803583b7b39dc3dac4ddcdc97cdeff52c
Joshua Carroll <carrolljosh1976@gmail.com>
2024-03-04 19:05:09 -0500

    Made image on GameOverScreen dynamic based on screen dimensions,made component scrollable

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,29 +1,49 @@
-import { Image, StyleSheet, Text, View,Dimensions } from "react-native"
+import { Image,ScrollView, StyleSheet, Text, View,useWindowDimensions } from "react-native"
 
 import Title from "../components/ui/Title"
 import Colors from "../utils/colors"
 import PrimaryButton from "../components/ui/PrimaryButton"
 
 export default function GameOverScreen({roundsNumber, userNumber, onStartNewGame}) {
+    const {width,height} = useWindowDimensions();
+
+    let imageSize = 300;
+
+    if(width < 380) {
+        imageSize = 150;
+    }
+
+    if(height < 400) {
+        imageSize = 80;
+    }
+
+    const imageStyle = {
+        width: imageSize,
+        height: imageSize,
+        borderRadius: imageSize / 2
+    }
+
     return (
-        <View style = {styles.rootContainer}>
-            <Title>GAME OVER!</Title>
-            <View style = {styles.imageContainer}>
-                <Image
-                    style = {styles.image}
-                    source={require("../assets/images/success.png")} 
-                />
+        <ScrollView>
+            <View style = {styles.rootContainer}>
+                <Title>GAME OVER!</Title>
+                <View style = {[styles.imageContainer,imageStyle]}>
+                    <Image
+                        style = {styles.image}
+                        source={require("../assets/images/success.png")} 
+                    />
+                </View>
+                <Text style = {styles.summaryText}>
+                    Your device needed <Text style ={styles.highlight}>{roundsNumber} </Text>
+                    rounds to guess the number <Text style ={styles.highlight}>{userNumber}</Text>.
+                </Text>
+                <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
             </View>
-            <Text style = {styles.summaryText}>
-                Your device needed <Text style ={styles.highlight}>{roundsNumber} </Text>
-                rounds to guess the number <Text style ={styles.highlight}>{userNumber}</Text>.
-            </Text>
-            <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
-        </View>
+        </ScrollView>
     )
 }
 
-const deviceWidth = Dimensions.get("window").width;
+//const deviceWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
     rootContainer: {
@@ -33,9 +53,9 @@ const styles = StyleSheet.create({
         alignItems: "center"
     },
     imageContainer: {
-        height: deviceWidth < 380 ? 150 : 300,
-        width: deviceWidth < 380 ? 150 : 300,
-        borderRadius: deviceWidth < 380 ? 75 : 150,
+       // height: deviceWidth < 380 ? 150 : 300,
+        //width: deviceWidth < 380 ? 150 : 300,
+        //borderRadius: deviceWidth < 380 ? 75 : 150,
         borderWidth: 3,
         borderColor: Colors.primary800,
         overflow: "hidden",
